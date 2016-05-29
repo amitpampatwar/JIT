ee53c6ee86d7a0909bed500be5e4984e0a7be482
amitpampatwar <amitascent@gmail.com>
2016-05-30 00:44:16 +0530

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -1,30 +1,41 @@
-﻿$(document).ready(function(){
+$(document).ready(function(){
   addfooter();  
 });
 
-function addfooter(footer_image)
+function addfooter()
 {  
-  if ( document.getElementById )
-  {
-    var footer = document.getElementById ( 'footer' );
+  $.ajax({ 
+    type: 'GET', 
+    url: './json/menu.json',
+    dataType: "text",
+    async: false,
+    success: function (data) {
+      var footer = document.getElementById('footerList');
+      var objMenu = jQuery.parseJSON(data);
+      var footerHTML = "";
+      
+      $.each(objMenu, function(index, objParent) {
+        if (objParent.ChildNav !== undefined) {
+          footerHTML = footerHTML + "<li class=\"uk-parent\"><a href=\"" + objParent.Link + "\">" + objParent.Name + "</a>";
+          footerHTML = footerHTML + "<ul class=\"uk-nav-sub\">";
+          
+          $.each(objParent.ChildNav, function(childIndex, objChild) {
+            if (objChild.SubChildNav !== undefined) {
+              $.each(objChild.SubChildNav, function(subChildIndex, objSubChild) {
+                footerHTML = footerHTML + "<li><a href=\"" + objSubChild.Link + "\">" + objSubChild.Name + "</a></li>";
+              });
+            }
+            else {
+              footerHTML = footerHTML + "<li><a href=\"" + objChild.Link + "\">" + objChild.Name + "</a></li>";
+            }
+          });
 
-    if ( footer )
-    {
-      $.ajax({ 
-        type: 'GET', 
-        url: './contents/footer_contents.txt',
-        dataType: "text",
-        async: false,
-        success: function (footer_contents) {
-          if (footer_contents)
-          {
-            footer_contents = footer_contents.replace ('{{FooterImage}}', "images/SiteLogo.png" );
-            footer_contents = footer_contents.replace ('{{DisplayFooterImage}}', 'block' );
-            
-            footer.outerHTML = footer_contents;
-          }
+          footerHTML = footerHTML + "</ul></li>";
+        }
+        else {
+          footerHTML = footerHTML + "<li class=\"uk-parent\"><a href=\"" + objParent.Link + "\">" + objParent.Name + "</a></li>";
         }
       });
     }
-  }  
+  });
 }
