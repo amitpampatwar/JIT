998580235405de39ce6e6c226ae59eb105e3ceeb
amitpampatwar <amitascent@gmail.com>
2016-06-01 18:54:19 +0530

    Add files via upload

--- js/blog.js
+++ js/blog.js
@@ -12,12 +12,14 @@ function addData(url, headerType, elementName)
 
       $.each(objBlog, function(index, objData) {
         blogData = blogData + "<article class=\"uk-article\">";
-        blogData = blogData + "<h" + headerType + " class=\"uk-article-title\">" + objData.Title + "</h" + headerType + "><hr class=\"uk-article-divider\">";
+        blogData = blogData + "<h" + headerType + " class=\"uk-article-title\">" + objData.Title + "</h" + headerType + ">";
 
         if (objData.CreatedDate) {
           blogData = blogData + "<p class=\"uk-text-muted uk-float-right\">" + objData.CreatedDate + "</p>";
         }
-
+        
+        blogData = blogData + "<hr class=\"uk-article-divider\">";
+         
         $.each(objData.Content, function(index, objContent) {
           if (objContent.Form) {
             var formData = "";
