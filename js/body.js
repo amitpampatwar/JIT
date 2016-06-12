07cfbde6d1064422bcf24ee1317623108ef4bcec
amitpampatwar <amitascent@gmail.com>
2016-06-12 13:58:22 +0530

    Add files via upload

---
+++ js/body.js
@@ -0,0 +1,32 @@
+var folderNames = "json/folderNames.json";
+var objEvents;
+
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET', 
+    url: folderNames,
+    dataType: "text",
+    async: false,
+    success: function(data) {
+      var objFiles = jQuery.parseJSON(data);
+
+      $.each(objFiles, function(index, objData) {
+        GetAllEvents("json/" + objData.Name + "/fileDetails.json", function(eventData) {
+          objEvents.push(eventData);
+        });                          
+      });
+    }
+  });    
+});
+
+function GetAllEvents(fileName, callback) {
+  $.ajax({
+    type: 'GET', 
+    url: fileName,
+    dataType: "text",
+    async: false,
+    success: function(data) {
+      callback(jQuery.parseJSON(data));        
+    }
+  });
+}
