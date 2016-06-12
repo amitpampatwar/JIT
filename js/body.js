ada1d1cb35bc6c52898f88757954126d053dbd38
amitpampatwar <amitascent@gmail.com>
2016-06-12 16:07:16 +0530

    Add files via upload

--- js/body.js
+++ js/body.js
@@ -41,6 +41,8 @@ function AddEventData(objEvents) {
         if (allFiles.ImageURL) {
           var fileURL = eventFiles.Path.replace("json/", "");
           
+          fileURL = fileURL.replace(".json", "");
+          
           latestEventData = latestEventData + "<li>";
           latestEventData = latestEventData + "<img src=\"" + allFiles.ImageURL[0].URL + "\" width=\"800\" height=\"400\" alt=\"" + allFiles.ImageURL[0].ImageTitle + "\">";
           latestEventData = latestEventData + "<div class=\"uk-overlay-panel uk-overlay-background uk-overlay-bottom uk-overlay-slide-bottom\"><p>";
