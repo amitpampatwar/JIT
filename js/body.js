11f9026c76dc87b648a111f6f5901e7186919cd1
amitpampatwar <amitascent@gmail.com>
2016-06-12 22:29:32 +0530

    Add files via upload

--- js/body.js
+++ js/body.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
           $.each(eventData, function(index, eventFiles) {
             var fileWithoutPath = eventFiles.Filename.split("/");
             
-            objAll.push({ "Name" : fileWithoutPath[2], "Path" : eventFiles.Filename});
+            objAll.Push({ "Name" : fileWithoutPath[2], "Path" : eventFiles.Filename});
           });          
         });                          
       });
